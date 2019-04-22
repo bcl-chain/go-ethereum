6d17c33672482b42be766f822796944cf17d4073
Keisuke Katsushima <k-katsushima@nri.co.jp>
2019-04-22 13:03:03 +0800

    modify console package

--- console/console_test.go
+++ console/console_test.go
@@ -98,6 +98,7 @@ func newTester(t *testing.T, confOverride func(*eth.Config)) *tester {
 	ethConf := &eth.Config{
 		Genesis:   core.DeveloperGenesisBlock(15, common.Address{}),
 		Etherbase: common.HexToAddress(testAddress),
+		Validator: common.HexToAddress(testAddress),
 		Ethash: ethash.Config{
 			PowMode: ethash.ModeTest,
 		},
@@ -170,6 +171,9 @@ func TestWelcome(t *testing.T) {
 	}
 	if want := fmt.Sprintf("instance: %s", testInstance); !strings.Contains(output, want) {
 		t.Fatalf("console output missing instance: have\n%s\nwant also %s", output, want)
+	}
+	if want := fmt.Sprintf("validator: %s", testAddress); !strings.Contains(output, want) {
+		t.Fatalf("console output missing validator: have\n%s\nwant also %s", output, want)
 	}
 	if want := fmt.Sprintf("coinbase: %s", testAddress); !strings.Contains(output, want) {
 		t.Fatalf("console output missing coinbase: have\n%s\nwant also %s", output, want)
